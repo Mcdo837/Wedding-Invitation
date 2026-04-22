26fedce64f3b9dec037ed2419d694b509f92d5a3
krypto <mcdonaldkhonje220@gmail.com>
2026-04-22 15:23:10 +0200

    bro's js

--- index.js
+++ index.js
@@ -1,60 +1,56 @@
-const images = ['images/image1.jpeg','images/image2.jpeg','images/image3.jpeg','images/image4.jpeg','images/image5.jpeg',
-'images/image6.jpeg','images/image7.jpeg','images/image8.jpeg','images/image9.jpeg'];
-let count = 0,
-image_holder = document.getElementById("image-holder");
-image_holder.src = images[count];
-let total_images = images.length;
-
-setInterval(()=>{
+    const images = [
+      'image1.jpeg','image2.jpeg','image3.jpeg','image4.jpeg','image5.jpeg',
+      'image6.jpeg','image7.jpeg','image8.jpeg','image9.jpeg'
+    ];
+    let count = 0,
+    image_holder = document.getElementById("image-holder");
     image_holder.src = images[count];
-count++;
-if(count >= total_images){
-    count = 1;
-}else if(count < 1){
-    count = total_images;
-}
-},4000);
+    let total_images = images.length;
 
-// Initialize EmailJS
-   (function(){
+    setInterval(()=>{
+      image_holder.src = images[count];
+      count++;
+      if(count >= total_images){
+        count = 0;
+      }
+    },4000);
+
+    // Initialize EmailJS
+    (function(){
       emailjs.init({
         publicKey: "w4b8SBE-WPxbvodiw",
       });
-   })();
+    })();
 
-document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
+      emailjs.init("w4b8SBE-WPxbvodiw");
+      const button = document.querySelector(".submit-button");
 
-    emailjs.init("w4b8SBE-WPxbvodiw");
-
-    const button = document.querySelector(".submit-button");
-
-    button.addEventListener("click", function() {
-        button.textContent = `sending.....`;
+      button.addEventListener("click", function() {
+        button.textContent = sending.....;
         const fullName = document.getElementById("fullName").value;
         const phone = document.getElementById("phone").value;
         const guest1 = document.getElementById("guest1").value;
         const guest2 = document.getElementById("guest2").value;
 
         var templateParams = {
-            full_name: fullName,
-            phone_number: phone,
-            guest_one: guest1,
-            guest_two: guest2
+          full_name: fullName,
+          phone_number: phone,
+          guest_one: guest1,
+          guest_two: guest2
         };
 
         emailjs.send("service_0w19jzw", "template_gjbst9q", templateParams)
-            .then(() => {
-                alert("Your Invitation form has been sent");
-                button.textContent = "submit";
-                fullName = document.getElementById("fullName").value = "";
-                phone = document.getElementById("phone").value = "";
-                guest1 = document.getElementById("guest1").value = "";
-                guest2 = document.getElementById("guest2").value = "";
-            })
-            .catch(() => {
-                console.log("check your network!");
-                console.log("Onset is the best");
-            });
-
+          .then(() => {
+            alert("Your Invitation form has been sent");
+            button.textContent = "Submit";
+            document.getElementById("fullName").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("guest1").value = "";
+            document.getElementById("guest2").value = "";
+          })
+          .catch(() => {
+            console.log("Check your network!");
+          });
+      });
     });
-});
